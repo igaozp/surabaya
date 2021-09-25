2bf4192b6485aa26bd4191a8e3e5c7ca57ed600e
igaozp <igaozp@hotmail.com>
2021-09-25 20:43:21 +0800

    refactor: 更新实现写法
    BREAKING CHANGE: Gradle 增加 Java 版本设置

--- feature/src/main/java/language/RandomAccessFileExercise.java
+++ feature/src/main/java/language/RandomAccessFileExercise.java
@@ -1,12 +1,9 @@
 package language;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.Files;
-import java.nio.file.Path;
 
 /**
  * Compare two files content by memory-mapped file
@@ -16,23 +13,23 @@ import java.nio.file.Path;
  */
 public class RandomAccessFileExercise {
     public static void main(String[] args) throws IOException {
-        Path firstPath = Files.createTempFile("file1Test", ".txt");
-        Path secondPath = Files.createTempFile("file2Test", ".txt");
+        var firstPath = Files.createTempFile("file1Test", ".txt");
+        var secondPath = Files.createTempFile("file2Test", ".txt");
 
         Files.writeString(firstPath, "testing line 1" + System.lineSeparator() + "line 2");
         Files.writeString(secondPath, "testing line 1" + System.lineSeparator() + "line 2");
 
-        try (RandomAccessFile randomAccessFile1 = new RandomAccessFile(firstPath.toFile(), "r");
-             RandomAccessFile randomAccessFile2 = new RandomAccessFile(secondPath.toFile(), "r")) {
+        try (var randomAccessFile1 = new RandomAccessFile(firstPath.toFile(), "r");
+             var randomAccessFile2 = new RandomAccessFile(secondPath.toFile(), "r")) {
 
-            FileChannel firstChannel = randomAccessFile1.getChannel();
-            FileChannel secondChannel = randomAccessFile2.getChannel();
+            var firstChannel = randomAccessFile1.getChannel();
+            var secondChannel = randomAccessFile2.getChannel();
             if (firstChannel.size() != secondChannel.size()) {
                 System.out.println("files size mismatch");
             }
-            long size = firstChannel.size();
-            MappedByteBuffer firstBuffer = firstChannel.map(FileChannel.MapMode.READ_ONLY, 0L, size);
-            MappedByteBuffer secondBuffer = secondChannel.map(FileChannel.MapMode.READ_ONLY, 0L, size);
+            var size = firstChannel.size();
+            var firstBuffer = firstChannel.map(FileChannel.MapMode.READ_ONLY, 0L, size);
+            var secondBuffer = secondChannel.map(FileChannel.MapMode.READ_ONLY, 0L, size);
 
             System.out.println(firstBuffer.equals(secondBuffer));
         }
