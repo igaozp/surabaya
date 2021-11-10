f8cc3573759ac22f63801e54edde563f6706a1ec
igaozp <igaozp@hotmail.com>
2021-11-10 21:48:22 +0800

    feat: 新增 Spring 示例模块 & SSE、NDJSON 请求响应示例

---
+++ spring/src/main/java/xyz/andornot/controller/StreamJsonController.java
@@ -0,0 +1,31 @@
+package xyz.andornot.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.time.Instant;
+
+@RestController
+public class StreamJsonController {
+    @GetMapping(value = "sse", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    Flux<Data> sse() {
+        return dataStream();
+    }
+
+    @GetMapping(value = "ndjson", produces = MediaType.APPLICATION_NDJSON_VALUE)
+    Flux<Data> ndjson() {
+        return dataStream();
+    }
+
+    private Flux<Data> dataStream() {
+        return Flux.interval(Duration.ofSeconds(1))
+                .take(5)
+                .map(i -> new Data(i, Instant.now()));
+    }
+
+    public record Data(Long seqNo, Instant timestamp) {
+    }
+}
