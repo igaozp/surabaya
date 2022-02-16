b291fdbd8fe6dcbc3bfb0165d7deab04a924b213
igaozp <igaozp@hotmail.com>
2022-02-16 20:54:42 +0800

    feat: Redis Stream 示例 - xread 读取

--- redistream/src/main/java/xyz/andornot/config/RedisConfig.java
+++ redistream/src/main/java/xyz/andornot/config/RedisConfig.java
@@ -1,12 +1,9 @@
 package xyz.andornot.config;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.stereotype.Component;
 
 /**
@@ -18,13 +15,11 @@ public class RedisConfig {
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        var serializer = new Jackson2JsonRedisSerializer<>(Object.class);
-        var objectMapper = new ObjectMapper();
-        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        serializer.setObjectMapper(objectMapper);
         var redisTemplate = new RedisTemplate<String, Object>();
         redisTemplate.setConnectionFactory(redisConnectionFactory);
-        redisTemplate.setDefaultSerializer(serializer);
+        redisTemplate.setDefaultSerializer(RedisSerializer.string());
+        // 防止 stream 读取异常
+        redisTemplate.setHashValueSerializer(RedisSerializer.string());
         return redisTemplate;
     }
 }
---
+++ redistream/src/main/java/xyz/andornot/service/XreadNonBlockConsumer.java
@@ -0,0 +1,69 @@
+package xyz.andornot.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.data.redis.connection.stream.ReadOffset;
+import org.springframework.data.redis.connection.stream.StreamOffset;
+import org.springframework.data.redis.connection.stream.StreamReadOptions;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+import xyz.andornot.domain.Person;
+
+import javax.annotation.Resource;
+import java.time.Duration;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * xread 读取 stream 数据，读取后不会删除数据
+ * 每次启动可重复读取之前的数据
+ *
+ * @author igaozp
+ * @since 2022/2/16
+ */
+@Slf4j
+@Component
+public class XreadNonBlockConsumer implements InitializingBean, DisposableBean {
+    private ThreadPoolExecutor threadPoolExecutor;
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
+    private volatile boolean stop = false;
+
+    @Override
+    public void destroy() {
+        stop = true;
+        threadPoolExecutor.shutdown();
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        threadPoolExecutor = new ThreadPoolExecutor(1, 1, 0, TimeUnit.SECONDS, new LinkedBlockingDeque<>(), r -> {
+            Thread thread = new Thread(r);
+            thread.setDaemon(true);
+            thread.setName("xread-nonblock");
+            return thread;
+        });
+
+        var streamReadOptions = StreamReadOptions.empty().block(Duration.ofSeconds(1)).count(10);
+
+        var readOffset = new StringBuilder("0-0");
+        threadPoolExecutor.execute(() -> {
+            while (!stop) {
+                var records = redisTemplate.opsForStream()
+                        .read(Person.class, streamReadOptions, StreamOffset.create("STREAM_01", ReadOffset.from(readOffset.toString())));
+                if (CollectionUtils.isEmpty(records)) {
+                    log.warn("未读取到数据");
+                    continue;
+                }
+                records.forEach(r -> {
+                    log.info("读取到数据：{}", r);
+                    readOffset.setLength(0);
+                    readOffset.append(r.getId());
+                });
+            }
+        });
+    }
+}
