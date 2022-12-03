3a47576434daefe8329b7030fa1f45ccf65562bd
igaozp <igaozp@hotmail.com>
2022-12-03 15:22:19 +0800

    style: 增加说明

---
+++ design-pattern/src/main/java/xyz/andornot/creational/abstractfactory/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * https://refactoringguru.cn/design-patterns/abstract-factory
+ *
+ * @author igaozp
+ * @since 2022/12/3
+ */
+package xyz.andornot.creational.abstractfactory;
---
+++ design-pattern/src/main/java/xyz/andornot/creational/factorymethod/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * https://refactoringguru.cn/design-patterns/factory-method
+ *
+ * @author igaozp
+ * @since 2022/12/3
+ */
+package xyz.andornot.creational.factorymethod;
