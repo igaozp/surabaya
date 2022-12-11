cd059fea08a03308f8f5b8760e743e9b17858120
igaozp <igaozp@hotmail.com>
2022-12-11 16:22:35 +0800

    feat: 单例模式完善

--- design-pattern/src/main/java/xyz/andornot/creational/singleton/package-info.java
+++ design-pattern/src/main/java/xyz/andornot/creational/singleton/package-info.java
@@ -1,5 +1,6 @@
 /**
  * https://refactoringguru.cn/design-patterns/singleton
+ * https://www.digitalocean.com/community/tutorials/java-singleton-design-pattern-best-practices-examples
  *
  * @author igaozp
  * @since 2022/12/11
