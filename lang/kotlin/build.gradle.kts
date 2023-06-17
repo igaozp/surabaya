09946dc8afb144b4d747d5623d0c6fcf236fbc0a
igaozp <igaozp@hotmail.com>
2023-06-17 13:08:49 +0800

    feat: 优化 lang:kotlin 模块依赖

--- lang/kotlin/build.gradle.kts
+++ lang/kotlin/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.8.10"
+    kotlin("jvm") version "1.8.22"
 }
 
 group = "xyz.andornot"
@@ -10,8 +10,6 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
 
 tasks.getByName<Test>("test") {
