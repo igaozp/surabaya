514ab7244242925fd76e291386180e5631040a24
igaozp <igaozp@hotmail.com>
2023-07-01 16:00:48 +0800

    feat: [arch:tdd] 异步逻辑测试重构

--- arch/tdd/src/test/java/xyz/andornot/scholarship/notify/SendResultEmailServiceTest.java
+++ arch/tdd/src/test/java/xyz/andornot/scholarship/notify/SendResultEmailServiceTest.java
@@ -1,23 +1,26 @@
 package xyz.andornot.scholarship.notify;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.stubbing.OngoingStubbing;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 class SendResultEmailServiceTest {
+    private Mailer mailer = mock(Mailer.class);
+    private List<Future<Boolean>> futures;
+    private SendResultEmailService service = new SendResultEmailService(mailer);
+
     @Test
     void when_send_returns_future() throws ExecutionException, InterruptedException {
-        Mailer mailer = mock(Mailer.class);
-        SendResultEmailService service = new SendResultEmailService(mailer);
-
         when(mailer.send(any(ScholarshipResult.class))).thenReturn(true, true, false);
 
-        List<Future<Boolean>> futures = service.send(
+        futures = service.send(
                 List.of(
                         new ScholarshipResult(),
                         new ScholarshipResult(),
@@ -35,7 +38,51 @@ class SendResultEmailServiceTest {
             }
         }
 
-        Assertions.assertEquals(2, goods);
-        Assertions.assertEquals(1, bads);
+        assertEquals(2, goods);
+        assertEquals(1, bads);
+    }
+
+    @Test
+    void when_send_returns_future_refactor_the_test() throws ExecutionException, InterruptedException {
+        assume_mailer_execution_result_would_be(true, true, false);
+
+        when_send_with_results(3);
+
+        then_counts_in_futures_will_be(true, 2);
+        then_counts_in_futures_will_be(false, 1);
+    }
+
+    private void assume_mailer_execution_result_would_be(boolean first, boolean... followings) {
+        OngoingStubbing<Boolean> stubbing = when(mailer.send(any(ScholarshipResult.class))).thenReturn(first);
+
+        for (boolean expected : followings) {
+            stubbing.thenReturn(expected);
+        }
+    }
+
+    private void when_send_with_results(int number) {
+        futures = this.service.send(results(number));
+    }
+
+    private List<ScholarshipResult> results(int numbers) {
+        var results = new ArrayList<ScholarshipResult>(numbers);
+        for (int i = 0; i < numbers; i++) {
+            results.add(new ScholarshipResult());
+        }
+        return results;
+    }
+
+    private void then_counts_in_futures_will_be(boolean target, int expected) throws ExecutionException, InterruptedException {
+        assertEquals(expected, count_in_futures(target));
+    }
+
+    private int count_in_futures(boolean target) throws ExecutionException, InterruptedException {
+        var counts = 0;
+        for (Future<Boolean> future : futures) {
+            if (future.get() == target) {
+                counts++;
+            }
+        }
+        return counts;
     }
 }
