76fdfdc53d3047062a8e7da0a9e572f7516c7dba
igaozp <igaozp@hotmail.com>
2023-12-26 21:38:40 +0800

    feat(lang:java): threadlocal examples

---
+++ lang/java/src/main/java/lang/thread/threadlocal/SampleThreadLocalExample.java
@@ -0,0 +1,30 @@
+package lang.thread.threadlocal;
+
+public class SampleThreadLocalExample {
+    public static ThreadLocal<String> CONTEXT = ThreadLocal.withInitial(() -> null);
+    public static InheritableThreadLocal<String> INHERITABLE_CONTEXT = new InheritableThreadLocal<>();
+
+    static void insideParentThread() {
+        System.out.println(STR."ThreadLocal Value in insideParentThread(): \{CONTEXT.get()}");
+        System.out.println(STR."InheritableThreadLocal Value in insideParentThread(): \{INHERITABLE_CONTEXT.get()}");
+    }
+
+    static void insideChildThread() {
+        System.out.println(STR."ThreadLocal Value in insideChildThread(): \{CONTEXT.get()}");
+        System.out.println(STR."InheritableThreadLocal Value in insideChildThread(): \{INHERITABLE_CONTEXT.get()}");
+    }
+
+    public static void main(String[] args) {
+        Thread parentThread = new Thread(() -> {
+            CONTEXT.set("TestValue");
+            INHERITABLE_CONTEXT.set("TestValue");
+            insideParentThread();
+
+            var childThread = new Thread(SampleThreadLocalExample::insideChildThread);
+            childThread.start();
+            insideParentThread();
+        });
+
+        parentThread.start();
+    }
+}
