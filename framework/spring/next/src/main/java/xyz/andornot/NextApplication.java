b28949b0d24c35d87d330e78ce8e21580c3d4f10
igaozp <igaozp@hotmail.com>
2023-12-27 21:21:29 +0800

    feat(framework:spring:next): enable virtual threads

---
+++ framework/spring/next/src/main/java/xyz/andornot/NextApplication.java
@@ -0,0 +1,11 @@
+package xyz.andornot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NextApplication.class, args);
+    }
+}
