b2398bf9d0a529dcd44ac91b0f0ecdc4edfe59ea
igaozp <igaozp@hotmail.com>
2024-01-27 22:03:06 +0800

    feat: add framework:spring:connection-pool module

---
+++ framework/spring/connection-pool/src/main/java/xyz/andornot/connectionpool/ConnectionPoolApplication.java
@@ -0,0 +1,13 @@
+package xyz.andornot.connectionpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectionPoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConnectionPoolApplication.class, args);
+    }
+
+}
