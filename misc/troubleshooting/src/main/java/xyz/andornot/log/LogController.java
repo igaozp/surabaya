61a288c9e108346c6788da5771ef3dacc3bb27b5
igaozp <igaozp@hotmail.com>
2024-03-26 22:32:03 +0800

    feat: troubleshooting log json endpoint example

--- misc/troubleshooting/src/main/java/xyz/andornot/log/LogController.java
+++ misc/troubleshooting/src/main/java/xyz/andornot/log/LogController.java
@@ -1,5 +1,6 @@
 package xyz.andornot.log;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +12,10 @@ public class LogController {
     @RequestMapping("print")
     public String print(@RequestParam(name = "info") String info) {
         return "Print some log and info:" + info;
+    }
+
+    @RequestMapping("printJson")
+    public String printJson(@RequestBody String info) {
+        return "Print some log and json info:" + info;
     }
 }
--- misc/troubleshooting/src/test/java/xyz/andornot/log/LogControllerTest.java
+++ misc/troubleshooting/src/test/java/xyz/andornot/log/LogControllerTest.java
@@ -4,6 +4,7 @@ import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -36,5 +37,15 @@ public class LogControllerTest {
     public void printLogWithoutInfo() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/log/print"))
                 .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void printJsonWithValidInfo() throws Exception {
+        String validJson = "{\"key\":\"value\"}";
+        mockMvc.perform(MockMvcRequestBuilders.post("/log/printJson")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(validJson))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Print some log and json info:" + validJson));
     }
 }
