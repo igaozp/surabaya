a355a7d000107df08803d6c2df5da9364601724e
igaozp <igaozp@hotmail.com>
2025-11-28 22:36:55 +0800

    refactor(esclient): replace RestClient with ElasticsearchClient for improved configuration

--- db/elasticsearch/src/main/java/xyz/andornot/config/EsClientConfig.java
+++ db/elasticsearch/src/main/java/xyz/andornot/config/EsClientConfig.java
@@ -2,14 +2,9 @@ package xyz.andornot.config;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
-import co.elastic.clients.transport.rest_client.RestClientTransport;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import org.apache.http.Header;
-import org.apache.http.HttpHost;
-import org.apache.http.message.BasicHeader;
-import org.apache.http.ssl.SSLContexts;
-import org.elasticsearch.client.RestClient;
+import org.apache.hc.core5.ssl.SSLContexts;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
@@ -50,18 +45,18 @@ public class EsClientConfig {
         var sslContextBuilder = SSLContexts.custom()
                 .loadTrustMaterial(trustStore, null);
         final var sslContext = sslContextBuilder.build();
-        var restClient = RestClient.builder(HttpHost.create(url))
-                .setDefaultHeaders(new Header[]{
-                        new BasicHeader("Authorization", "ApiKey " + apiKey)
-                })
-                .setHttpClientConfigCallback(builder -> builder.setSSLContext(sslContext)).build();
 
-        // Create the transport with a Jackson mapper
         var objectMapper = new ObjectMapper();
         objectMapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
         var jsonpMapper = new JacksonJsonpMapper(objectMapper);
-        var transport = new RestClientTransport(restClient, jsonpMapper);
 
-        return new ElasticsearchClient(transport);
+        var esClient = ElasticsearchClient.of(b -> b
+                .host(url)
+                .apiKey(apiKey)
+                .sslContext(sslContext)
+                .jsonMapper(jsonpMapper)
+        );
+
+        return esClient;
     }
 }
