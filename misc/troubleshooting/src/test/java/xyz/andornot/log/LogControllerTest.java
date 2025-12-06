215624f57848cc902cab0198605b38d809361f2f
igaozp <igaozp@hotmail.com>
2025-12-06 22:43:16 +0800

    build(deps): update import for AutoConfigureMockMvc in LogControllerTest

--- misc/troubleshooting/src/test/java/xyz/andornot/log/LogControllerTest.java
+++ misc/troubleshooting/src/test/java/xyz/andornot/log/LogControllerTest.java
@@ -2,8 +2,8 @@ package xyz.andornot.log;
 
 import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
